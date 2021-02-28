bf9b69e13124b5e02af42082ab4090d9dbee0b8a
Lyla <github.com/Liliana-EE>
2021-02-28 22:59:05 +0000

    Initialized a repo, added Module2.py, Updated Variables and Types exercises

--- Module2.py
+++ Module2.py
@@ -1,1 +1,53 @@
-print("second")
+print("Hello Python")
+# Example, do not modify!
+print(5 / 8)
+# Print the sum of 7 and 10
+print(7 + 10)
+# Addition
+print(7 + 10)
+# Addition, subtraction
+print(5 + 5)
+print(5 - 5)
+
+# Multiplication, division, modulo, and exponentiation
+print(3 * 5)
+print(10 / 2)
+print(18 % 7)
+print(4 ** 2)
+
+# How much is your $100 worth after 7 years?
+print(100 * 1.1 ** 7)
+
+# Reproducibility
+height = 1.79
+weight = 74.2
+BMI = weight / height ** 2
+print(BMI)
+
+# Create a variable savings
+savings = 100
+
+# Print out savings
+print(savings)
+
+# Create a variable savings
+savings = 100
+
+# Create a variable growth_multiplier
+growth_multiplier = 1.1
+
+# Calculate result
+result = savings * growth_multiplier ** 7
+
+# Print out result
+print(result)
+
+# Create a variable desc
+desc = "compound interest"
+
+# Create a variable profitable
+profitable = True
+
+# Find variable types
+type("compound interest")
+type(True)
