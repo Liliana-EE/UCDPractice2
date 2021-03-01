cfc33a3d7774b3a27570f2f12a05f1dd24011645
Lyla <github.com/Liliana-EE>
2021-03-01 22:00:26 +0000

    Updated M2 - Packages exercises

--- Module2.py
+++ Module2.py
@@ -234,3 +234,19 @@ areas.reverse()
 
 # Print out areas
 print(areas)
+
+# Definition of radius
+r = 0.43
+
+# Import the math package
+import math
+
+# Calculate C
+C = 2 * r * math.pi
+
+# Calculate A
+A = math.pi * r ** 2
+
+# Build printout
+print("Circumference: " + str(C))
+print("Area: " + str(A))
