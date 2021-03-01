630f2f818c911cac122e5ceee2905a00b0047e2f
Lyla <github.com/Liliana-EE>
2021-03-01 20:12:24 +0000

    Updated M2

--- Module2.py
+++ Module2.py
@@ -40,7 +40,7 @@ growth_multiplier = 1.1
 result = savings * growth_multiplier ** 7
 
 # Print out result
-print(result)
+print(round(result))
 
 # Create a variable desc
 desc = "compound interest"
@@ -49,5 +49,124 @@ desc = "compound interest"
 profitable = True
 
 # Find variable types
-type("compound interest")
-type(True)
+print(type("compound interest"))
+print(type(True))
+
+# Assign product of savings and growth_multiplier to year1
+year1 = savings * growth_multiplier
+
+# Print the type of year1
+print(type(year1))
+
+# Assign sum of desc and desc to doubledesc
+doubledesc = desc + desc
+
+# Print out doubledesc
+print(doubledesc)
+
+# Definition of savings and result
+savings = 100
+result = 100 * 1.10 ** 7
+
+# Fix the printout
+print("I started with $" + str(round(savings)) + " and now have $" + str(round(result)) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string)
+print(pi_string)
+
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Create list areas
+areas = [hall, kit, liv, bed, bath]
+
+# Print areas
+print(areas)
+
+# Adapt list areas
+areas = ["hallway", hall, "kitchen", kit, "living room", liv, "bedroom", bed, "bathroom", bath]
+
+# Print areas
+print(areas)
+
+# house information as list of lists
+house = [["hallway", hall],
+         ["kitchen", kit],
+         ["living room", liv],
+         ["bedroom", bed],
+         ["bathroom", bath]]
+
+# Print out house
+print(house)
+
+# Print out the type of house
+print(type(house))
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Print out second element from areas
+print(areas[1])
+
+# Print out last element from areas
+print(areas[-1])
+
+# Print out the area of the living room
+print(areas[5])
+
+# Sum of kitchen and bedroom area: eat_sleep_area
+eat_sleep_area = (areas[3]) + (areas[7])
+
+# Print the variable eat_sleep_area
+print(eat_sleep_area)
+
+# Use slicing to create downstairs
+downstairs = areas[0:6]
+
+# Alternative slicing to create upstairs
+upstairs = areas[6:]
+
+# Print out downstairs and upstairs
+print(downstairs)
+print(upstairs)
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Correct the bathroom area
+areas[-1] = 10.50
+
+# Change "living room" to "chill zone"
+areas[4] = "chill zone"
+print(areas)
+
+# Add poolhouse data to areas, new list is areas_1
+areas_1 = areas + ["poolhouse", 24.5]
+
+# Add garage data to areas_1, new list is areas_2
+areas_2 = areas_1 + ["garage", 15.45]
+print(areas_2)
+
+# Remove poolhouse and its area from areas_2
+del(areas_2[-4:-2])
+print(areas_2)
+
+# Create list areas_3
+areas_3 = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Create areas_copy
+areas_3_copy = list(areas)
+
+# Change areas_copy
+areas_3_copy[0] = 5.0
+
+# Print areas
+print(areas_3)
