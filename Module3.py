4fcb9c3b69169031daf0a8b64de2c81adaf5691c
Lyla <github.com/Liliana-EE>
2021-03-17 21:08:25 +0000

    Updated - M3 DCamp - Dictionaries to DataFrames

--- Module3.py
+++ Module3.py
@@ -44,7 +44,7 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 
-df =pd.read_csv('Gapminder.csv')
+df =pd.read_csv('Gapminder.csv', index_col =0)
 
 # how to define life_exp and gd_cap?
 
@@ -214,3 +214,72 @@ europe = { 'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'osl
 
 # Print europe
 print(europe)
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Add italy to europe
+europe["italy"]='rome'
+
+# Print out italy in europe
+print('italy'in europe)
+
+# Add poland to europe
+europe["poland"]='warsaw'
+
+# Print europe
+print(europe)
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'bonn',
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw',
+          'australia':'vienna' }
+
+# Update capital of germany
+europe['germany']='berlin'
+
+# Remove australia
+del(europe['australia'])
+
+# Print europe
+print(europe)
+
+# Dictionary of dictionaries
+europe = { 'spain': { 'capital':'madrid', 'population':46.77 },
+           'france': { 'capital':'paris', 'population':66.03 },
+           'germany': { 'capital':'berlin', 'population':80.62 },
+           'norway': { 'capital':'oslo', 'population':5.084 } }
+
+
+# Print out the capital of France
+print(europe['france']['capital'])
+
+# Create sub-dictionary data
+data = {'population': 59.83, 'capital': 'rome'}
+
+# Add data to europe under key 'italy'
+europe['italy'] = data
+
+# Print europe
+print(europe)
+
+# Pre-defined lists
+names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
+dr =  [True, False, False, False, True, True, True]
+cpc = [809, 731, 588, 18, 200, 70, 45]
+
+# Import pandas as pd
+import pandas as pd
+
+# Create dictionary my_dict with three key:value pairs: my_dict
+my_dict = {
+           'country':names,
+           'drives_right': dr,
+           'cars_per_cap': cpc
+           }
+
+# Build a DataFrame cars from my_dict: cars
+cars = pd.DataFrame(my_dict)
+
+# Print cars
+print(cars)
