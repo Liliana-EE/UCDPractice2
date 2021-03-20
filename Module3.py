577ece2e2a22fe0b3a0c2da6f972e4bd9a7c6a2b
Lyla <github.com/Liliana-EE>
2021-03-20 21:09:16 +0000

    Updated - M3 DCamp

--- Module3.py
+++ Module3.py
@@ -689,3 +689,15 @@ areas = [11.25, 18.0, 20.0, 10.75, 9.50]
 for index, a in enumerate(areas) :
     print("room " + str(index+1) + ": " + str(a))
 
+# house list of lists
+house = [["hallway", 11.25],
+         ["kitchen", 18.0],
+         ["living room", 20.0],
+         ["bedroom", 10.75],
+         ["bathroom", 9.50]]
+
+# Build a for loop from scratch
+for room, area in house:
+    print("the " + str(room) + " is " + str(area) + " sqm")
+
+
