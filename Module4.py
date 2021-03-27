67ff57accd0a6976e5866a411c465ad8be2ad627
Lyla <github.com/Liliana-EE>
2021-03-27 17:19:11 +0000

    Updated - M4 DCamp

--- Module4.py
+++ Module4.py
@@ -1,9 +1,9 @@
-#Introduction to Importing Data
+# Introduction to Importing Data
 # Importing entire text files
 
 # Open a file: file
 filename = 'moby_dick.txt'
-file = open(filename, mode= 'r')
+file = open(filename, mode='r')
 text = file.read()
 
 # Print it
@@ -18,7 +18,7 @@ file.close()
 # Check whether file is closed
 print(file.closed)
 
-#Importing text files line by line
+# Importing text files line by line
 
 # Read & print the first 3 lines
 with open('moby_dick.txt') as file:
@@ -26,8 +26,42 @@ with open('moby_dick.txt') as file:
     print(file.readline())
     print(file.readline())
 
-#Importing different datatypes
+# Importing different datatypes
 import matplotlib.pyplot as plt
+
+# Using NumPy to import flat files
+# Import package
+import numpy as np
+
+# Assign filename to variable: file
+file = 'digits.csv'
+
+# Load file as array: digits
+digits = np.loadtxt(file, delimiter=',')
+
+# Print datatype of digits
+print(type(digits))
+
+# Select and reshape a row
+im = digits[21, 1:]
+im_sq = np.reshape(im, (28, 28))
+
+# Plot reshaped data (matplotlib.pyplot already loaded as plt)
+plt.imshow(im_sq, cmap='Greys', interpolation='nearest')
+plt.show()
+
+# Customizing NumPy import
+# Import numpy
+import numpy as np
+
+# Assign the filename: file
+file = 'digits_header.txt'
+
+# Load the data: data
+data = np.loadtxt(file, delimiter='\t', skiprows=1, usecols=[0, 2])
+
+# Print data
+print(data)
 
 # Import numpy
 import numpy as np
@@ -53,7 +87,7 @@ plt.xlabel('time (min.)')
 plt.ylabel('percentage of larvae')
 plt.show()
 
-#Working with mixed datatypes
+# Working with mixed datatypes
 # Assign the filename: file
 file = 'Titanic.csv'
 
@@ -63,7 +97,9 @@ d = np.recfromcsv('Titanic.csv', delimiter=',', names=True, dtype=None)
 # Print out first three entries of d
 print(d[:3])
 
-#Importing flat files using pandas
+# Using pandas to import flat files as DataFrames
+
+# Importing flat files using pandas
 # Import pandas as pd
 import pandas as pd
 
@@ -75,3 +111,192 @@ df = pd.read_csv(file)
 
 # View the head of the DataFrame
 print(df.head())
+
+# Assign the filename: file
+file = 'digits.csv'
+
+# Read the first 5 rows of the file into a DataFrame: data
+data = pd.read_csv(file, nrows=5, header=None)
+
+# Build a numpy array from the DataFrame: data_array
+data_array = data.values
+
+# Print the datatype of data_array to the shell
+print(type(data_array))
+
+# Customizing pandas import
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Assign filename: file
+file = 'titanic_corrupt.txt'
+
+# Import file: data
+data = pd.read_csv(file, sep='\t', comment='#', na_values='Nothing')
+
+# Print the head of the DataFrame
+print(data.head())
+
+# Plot 'Age' variable in a histogram
+pd.DataFrame.hist(data[['Age']])
+plt.xlabel('Age (years)')
+plt.ylabel('count')
+plt.show()
+
+# Loading a pickled file
+# Import pickle package
+import pickle
+
+# Open pickle file and load data: d
+with open('data.pkl', 'rb') as file:
+    d = pickle.load(file)
+
+# Print d
+print(d)
+
+# Print datatype of d
+print(type(d))
+
+# Listing sheets in Excel files
+# Import pandas
+import pandas as pd
+
+# Assign spreadsheet filename: file
+file = 'battledeath.xlsx'
+
+# Load spreadsheet: xls
+xls = pd.ExcelFile(file)
+
+# Print sheet names
+print(xls.sheet_names)
+
+# Importing sheets from Excel
+# Load a sheet into a DataFrame by name: df1
+df1 = xls.parse('2004')
+
+# Print the head of the DataFrame df1
+print(df1.head())
+
+# Load a sheet into a DataFrame by index: df2
+df2 = xls.parse(0)
+
+# Print the head of the DataFrame df2
+print(df2.head())
+
+# Customizing spreadsheet import
+# Parse the first sheet and rename the columns: df1
+df1 = xls.parse(0, skiprows=[0], names=['Country', 'AAM due to War (2002)'])
+
+# Print the head of the DataFrame df1
+print(df1.head())
+
+# Parse the first column of the second sheet and rename the column: df2
+df2 = xls.parse(0, usecols=[0], skiprows=[0], names=['Country'])
+
+# Print the head of the DataFrame df2
+print(df2.head())
+
+# Importing SAS files
+# Import sas7bdat package
+from sas7bdat import SAS7BDAT
+
+# Save file to a DataFrame: df_sas
+with SAS7BDAT('sales.sas7bdat') as file:
+    df_sas = file.to_data_frame()
+
+# Print head of DataFrame
+print(df_sas.head())
+
+# Plot histogram of DataFrame features (pandas and pyplot already imported)
+pd.DataFrame.hist(df_sas[['P']])
+plt.ylabel('count')
+plt.show()
+
+# Importing Stata Files
+# Import pandas
+import pandas as pd
+
+# Load Stata file into a pandas DataFrame: df
+df = pd.read_stata('disarea.dta')
+
+# Print the head of the DataFrame df
+print(df.head())
+
+# Plot histogram of one column of the DataFrame
+pd.DataFrame.hist(df[['disa10']])
+plt.xlabel('Extent of disease')
+plt.ylabel('Number of countries')
+plt.show()
+
+# Importing h5py to HDF5
+
+# Import packages
+import numpy as np
+import h5py
+
+# Assign filename: file
+file = 'LIGO_data.hdf5'
+
+# Load file: data
+data = h5py.File(file, 'r')
+
+# Print the datatype of the loaded file
+print(type(file))
+
+# Print the keys of the file
+for key in data.keys():
+    print(key)
+
+# Extracting data from your HDF5 file
+
+# Get the HDF5 group: group
+group = data['strain']
+
+# Check out keys of group
+for key in group.keys():
+    print(key)
+
+# Set variable equal to time series data: strain
+strain = data['strain']['Strain'].value
+
+# Set number of time points to sample: num_samples
+num_samples = 10000
+
+# Set time vector
+time = np.arange(0, 1, 1 / num_samples)
+
+# Plot data
+plt.plot(time, strain[:num_samples])
+plt.xlabel('GPS Time (s)')
+plt.ylabel('strain')
+plt.show()
+
+# Importing MATLAB files
+# Import package
+import scipy.io
+
+# Load MATLAB file: mat
+filename = 'albeck_gene_expression.mat'
+mat = scipy.io.loadmat(filename)
+
+# Print the datatype type of mat
+print(type(mat))
+
+# The structure of .mat in Python
+# Print the keys of the MATLAB dictionary
+print(mat.keys())
+
+# Print the type of the value corresponding to the key 'CYratioCyt'
+print(type(mat['CYratioCyt']))
+
+# Print the shape of the value corresponding to the key 'CYratioCyt'
+print(np.shape(mat['CYratioCyt']))
+
+# Subset the array and plot it
+data = mat['CYratioCyt'][25, 5:]
+fig = plt.figure()
+plt.plot(data)
+plt.xlabel('time (min.)')
+plt.ylabel('normalized fluorescence (measure of expression)')
+plt.show()
