1630d830a4acb6de7b7b021b571e225c197c7715
Lyla <github.com/Liliana-EE>
2021-04-11 23:42:05 +0100

    Updated - M5 DCamp - Data manipulation with pandas

--- Module5.py
+++ Module5.py
@@ -157,3 +157,305 @@ print(result)
 # This is almost double the number of the next-highest state, Hawaii. If you combine new column addition,
 # row subsetting, sorting, and column selection, you can answer lots of questions like this.
 
+#Summary statistics
+#Mean and median
+
+import pandas as pd
+data=pd.read_csv('sales_subset.csv')
+sales=data
+
+# Print the head of the sales DataFrame
+print(sales.head())
+
+# Print the info about the sales DataFrame
+print(sales.info())
+
+# Print the mean of weekly_sales
+print(sales['weekly_sales'].mean())
+
+# Print the median of weekly_sales
+print(sales['weekly_sales'].median())
+
+#Sumarasing dates
+
+# Print the maximum of the date column
+print(sales['date'].max())
+
+# Print the minimum of the date column
+print(sales['date'].min())
+
+#Efficient summaries
+#1. Use the custom iqr function defined for you along with .agg() to print the IQR of
+# the temperature_c column of sales.
+
+# A custom IQR function
+def iqr(column):
+    return column.quantile(0.75) - column.quantile(0.25)
+
+
+# Print IQR of the temperature_c column
+print(sales['temperature_c'].agg(iqr))
+
+# 2. Update the column selection to use the custom iqr function with .agg() to print the IQR of temperature_c,
+# fuel_price_usd_per_l, and unemployment, in that order
+
+# A custom IQR function
+def iqr(column):
+    return column.quantile(0.75) - column.quantile(0.25)
+
+# Update to print IQR of temperature_c, fuel_price_usd_per_l, & unemployment
+print(sales[["temperature_c", 'fuel_price_usd_per_l','unemployment']].agg(iqr))
+
+#3. Update the aggregation functions called by .agg(): include iqr and np.median in that order.
+
+# Import NumPy and create custom IQR function
+import numpy as np
+def iqr(column):
+    return column.quantile(0.75) - column.quantile(0.25)
+
+# Update to print IQR and median of temperature_c, fuel_price_usd_per_l, & unemployment
+print(sales[["temperature_c", "fuel_price_usd_per_l", "unemployment"]].agg([iqr,np.median]))
+
+#The .agg() method makes it easy to compute multiple statistics on multiple columns, all in just one line of code.
+
+# Cumulative statistics
+
+# Counting categorical data
+# Dropping duplicates
+# Drop duplicate store/type combinations
+store_types = sales.drop_duplicates(subset=['store','type'])
+print(store_types.head())
+
+# Drop duplicate store/department combinations
+store_depts = sales.drop_duplicates(subset=['store','department'])
+print(store_depts.head())
+
+# Subset the rows where is_holiday is True and drop duplicate dates
+holiday_dates = sales[sales['is_holiday']].drop_duplicates(subset='date')
+
+# Print date col of holiday_dates
+print(holiday_dates['date'])
+
+#Counting categorical variables
+
+# Count the number of stores of each type
+store_counts = store_types['type'].value_counts()
+print(store_counts)
+
+# Get the proportion of stores of each type
+store_props = store_types['type'].value_counts(normalize=True)
+print(store_props)
+
+# Count the number of each department number and sort
+dept_counts_sorted = store_depts['department'].value_counts(sort=False)
+print(dept_counts_sorted)
+
+# Get the proportion of departments of each number and sort
+dept_props_sorted = store_depts['department'].value_counts(sort=True, normalize=True)
+print(dept_props_sorted)
+
+#Grouped summary statistics
+#What percent of sales occurred at each store type?
+
+# Calc total weekly sales
+sales_all = sales["weekly_sales"].sum()
+
+# Subset for type A stores, calc total weekly sales
+sales_A = sales[sales["type"] == "A"]["weekly_sales"].sum()
+
+# Subset for type B stores, calc total weekly sales
+sales_B = sales[sales["type"] == "B"]["weekly_sales"].sum()
+
+# Subset for type C stores, calc total weekly sales
+sales_C = sales[sales["type"] == "C"]["weekly_sales"].sum()
+
+# Get proportion for each type
+sales_propn_by_type = [sales_A, sales_B, sales_C] / sales_all
+print(sales_propn_by_type)
+
+#Calculations with .groupby()
+# Group by type; calc total weekly sales
+sales_by_type = sales.groupby("type")["weekly_sales"].sum()
+
+# Get proportion for each type
+sales_propn_by_type = sales_by_type / sum(sales.weekly_sales)
+print(sales_propn_by_type)
+
+# Group sales by "type" and "is_holiday", take the sum of weekly_sales, and store as sales_by_type_is_holiday.
+
+# From previous step
+sales_by_type = sales.groupby("type")["weekly_sales"].sum()
+
+# Group by type and is_holiday; calc total weekly sales
+sales_by_type_is_holiday = sales.groupby(['type','is_holiday'])["weekly_sales"].sum()
+print(sales_by_type_is_holiday)
+
+#Multiple grouped summaries
+
+# Import numpy with the alias np
+import numpy as np
+
+# For each store type, aggregate weekly_sales: get min, max, mean, and median
+sales_stats = sales.groupby('type')['weekly_sales'].agg([np.min,np.max,np.mean,np.median])
+
+# Print sales_stats
+print(sales_stats)
+
+# For each store type, aggregate unemployment and fuel_price_usd_per_l: get min, max, mean, and median
+unemp_fuel_stats = sales.groupby('type')[['unemployment','fuel_price_usd_per_l']].agg([np.min,np.max,np.mean,np.median])
+
+# Print unemp_fuel_stats
+print(unemp_fuel_stats)
+
+#PIVOT TABLES
+# Pivoting on one variable
+
+# Get the mean weekly_sales by type using .pivot_table() and store as mean_sales_by_type.
+
+# Pivot for mean weekly_sales for each store type
+mean_sales_by_type = sales.pivot_table(values='weekly_sales',index='type')
+
+# Print mean_sales_by_type
+print(mean_sales_by_type)
+
+# Get the mean and median (using NumPy functions) of weekly_sales by type using .pivot_table() and store as mean_med_sales_by_type.
+
+# Import NumPy as np
+import numpy as np
+
+# Pivot for mean and median weekly_sales for each store type
+mean_med_sales_by_type = sales.pivot_table(values='weekly_sales',index='type',aggfunc=[np.mean,np.median])
+
+# Print mean_med_sales_by_type
+print(mean_med_sales_by_type)
+
+# Get the mean of weekly_sales by type and is_holiday using .pivot_table() and store as mean_sales_by_type_holiday.
+# Pivot for mean weekly_sales by store type and holiday
+mean_sales_by_type_holiday = sales.pivot_table(values='weekly_sales',index='type', columns='is_holiday')
+
+# Print mean_sales_by_type_holiday
+print(mean_sales_by_type_holiday)
+
+#Fill in missing values and sum values with pivot tables
+# Print mean weekly_sales by department and type; fill missing values with 0
+print(sales.pivot_table(values='weekly_sales',index='department', columns='type',fill_value=0))
+
+# Print the mean weekly_sales by department and type; fill missing values with 0s; sum all rows and cols
+print(sales.pivot_table(values="weekly_sales", index="department", columns="type", fill_value=0,margins=True))
+
+#Slicing and Idexing
+
+import pandas as pd
+data=pd.read_csv('temperatures.csv')
+temperatures = data
+
+# Look at temperatures
+print(temperatures)
+
+# Index temperatures by city
+temperatures_ind = temperatures.set_index('city')
+
+# Look at temperatures_ind
+print(temperatures_ind)
+
+# Reset the index, keeping its contents
+print(temperatures_ind.reset_index())
+
+# Reset the index, dropping its contents
+print(temperatures_ind.reset_index(drop=True))
+
+#Subsetting with .loc[]
+
+# Create a list called cities that contains "Moscow" and "Saint Petersburg".
+# Use [] subsetting to filter temperatures for rows where the city column takes a value in the cities list.
+# Use .loc[] subsetting to filter temperatures_ind for rows where the city is in the cities list.
+
+# Make a list of cities to subset on
+cities = ['Moscow', 'Saint Petersburg']
+
+# Subset temperatures using square brackets
+print(temperatures[temperatures['city'].isin(cities)])
+
+# Subset temperatures_ind using .loc[]
+print(temperatures_ind.loc[cities])
+
+#Setting multi-level indexes
+#Set the index of temperatures to the "country" and "city" columns, and assign this to temperatures_ind.
+# Specify two country/city pairs to keep: "Brazil"/"Rio De Janeiro" and "Pakistan"/"Lahore", assigning to rows_to_keep.
+# Print and subset temperatures_ind for rows_to_keep using .loc[].
+
+# Index temperatures by country & city
+temperatures_ind = temperatures.set_index(['country','city'])
+
+# List of tuples: Brazil, Rio De Janeiro & Pakistan, Lahore
+rows_to_keep = [('Brazil','Rio De Janeiro'),('Pakistan','Lahore')]
+
+# Subset for rows to keep
+print(temperatures_ind.loc[rows_to_keep])
+
+#Sorting by index values
+# Sort temperatures_ind by index values
+print(temperatures_ind.sort_index())
+
+# Sort temperatures_ind by index values at the city level
+print(temperatures_ind.sort_index(level='city'))
+
+# Sort temperatures_ind by country then descending city
+print(temperatures_ind.sort_index(level=['country','city'],ascending=[True,False]))
+
+#Slicing and subsetting with .loc and .iloc
+#Slicing index values
+
+# Sort the index of temperatures_ind
+temperatures_srt = temperatures_ind.sort_index()
+
+# Subset rows from Pakistan to Russia
+print(temperatures_srt.loc['Pakistan':'Russia'])
+
+# Try to subset rows from Lahore to Moscow
+print(temperatures_srt.loc['Lahore':'Moscow'])
+
+# Subset rows from Pakistan, Lahore to Russia, Moscow
+print(temperatures_srt.loc[('Pakistan','Lahore'):('Russia','Moscow')])
+
+#Slicing in both directions
+
+# Subset rows from India, Hyderabad to Iraq, Baghdad
+print(temperatures_srt.loc[('India','Hyderabad'):('Iraq','Baghdad')])
+
+# Subset columns from date to avg_temp_c
+print(temperatures_srt.loc[:, 'date':'avg_temp_c'])
+
+# Subset in both directions at once
+print(temperatures_srt.loc[('India','Hyderabad'):('Iraq','Baghdad'), 'date':'avg_temp_c'])
+
+#Slicing time series
+# Use Boolean conditions to subset temperatures for rows in 2010 and 2011
+temperatures_bool = temperatures[(temperatures['date'] >= '2010-01-01') & (temperatures['date'] <= '2011-12-31')]
+print(temperatures_bool)
+
+# Set date as an index and sort the index
+temperatures_ind = temperatures.set_index('date').sort_index()
+# Use .loc[] to subset temperatures_ind for rows in 2010 and 2011
+print(temperatures_ind.loc['2010':'2011'])
+
+# Use .loc[] to subset temperatures_ind for rows from Aug 2010 to Feb 2011
+print(temperatures_ind.loc['2010-08':'2011-02'])
+
+#Subsetting by row/column number
+# Get 23rd row, 2nd column (index 22, 1)
+print(temperatures.iloc[22,1])
+
+# Use slicing to get the first 5 rows
+print(temperatures.iloc[0:5,:])
+
+# Use slicing to get columns 3 to 4
+print(temperatures.iloc[:,2:4])
+
+# Use slicing in both directions at once
+print(temperatures.iloc[0:5,2:4])
+
+
+
+
+
